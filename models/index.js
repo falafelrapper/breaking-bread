644cf37842fe7ac8cd17e320983da641b994df50
mgberrysd <mgberrysd@gmail.com>
2024-02-04 19:21:46 -0800

    added User model

--- models/index.js
+++ models/index.js
@@ -2,3 +2,9 @@ const User = require('./User');
 const Category = require('./Category');
 const Recipe = require('./Recipe');
 
+User.hasMany(Recipe, {
+    foreignKey: '',
+    onDelete: 'CASCADE'
+});
+
+Recipe.belongsTo()
