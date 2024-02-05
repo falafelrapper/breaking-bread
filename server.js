bbf42b33574336f080c448e9e043ef8599d8049f
Margaux <margaux.santos113@gmail.com>
2024-02-05 10:34:52 -0800

    server.js starting point

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // importing required modules
-const path = require('path'); 
+const path = require('path'); // file and directory path 
 const express = require('express'); // express frame work
 const session = require('express-session'); // manages user's sessions
 const exphbs = require('express-handlebars'); // handlebars templating engine
@@ -13,7 +13,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Session configuartion 
 const sess = {
+
     secret: 'Super secret secret',
     cookie: {
         maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
@@ -23,20 +25,31 @@ const sess = {
     store: new SequelizeStore({
       db: sequelize
     })
+
   };
 
+// Use session middleware
 app.use(session(sess));
 
+// Handlebars setup
 const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Middleware for parsing JSON and URL-encoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware for serving static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use the routes defined in the './controllers' directory
 app.use(routes);
 
+// Synchronize Sequelize models with the database
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    
+    // Start the Express server and listen on the specified port
+    app.listen(PORT, () => console.log('Now listening'));
+
+});
