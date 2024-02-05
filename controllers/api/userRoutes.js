00d9d9ec110f4fd18ca5557bb9fa69c73f20f0aa
mgberrysd <mgberrysd@gmail.com>
2024-02-05 11:06:54 -0800

    updated api routes to post new users/recipes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,5 +2,16 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.post('/', async (req, res) => {
+    try {
+      const newUser = await User.create({
+        ...req.body,
+      });
+  
+      res.status(200).json(new);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -53,6 +53,31 @@ router.get('/recipes', async (req, res) => {
     }
 });
 
+router.get('/recipes/:id', async (req, res) => {
+    try {
+        const recipeData = await Recipe.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Category,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const recipe = recipeData.get({ plain: true });
+
+        res.render('recipe', {
+            ...recipe,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.get('/users/:id', async (req, res) => {
     try {
         const userData = await User.findByPk(req.params.id, {
@@ -76,30 +101,30 @@ router.get('/users/:id', async (req, res) => {
 
 router.get('/profile', withAuth, async (req, res) => {
     try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [{ model: Recipe }],
-      });
-  
-      const user = userData.get({ plain: true });
-  
-      res.render('profile', {
-        ...user,
-        logged_in: true
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Recipe }],
+        });
 
-  router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
     }
-  
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
     res.render('login');
-  });
+});
 
 module.exports = router;
